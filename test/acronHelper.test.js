5c1c011cf3d1be49555a37f4872040c247a6ce30
CoderLim <779793115@qq.com>
2017-11-17 12:41:19 +0800

    :white_check_mark: jest test

---
+++ test/acronHelper.test.js
@@ -0,0 +1,9 @@
+
+import { propertyExists } from '../src/parseHelper.js';
+
+describe('Property Exists', () => {
+  it('should compare prop.key.name with propName', () => {
+    expect(propertyExists([{prop1: 'value1'}], 'prop1')).toEqual(false);
+    expect(propertyExists([{key: {name: 'prop1'}, value:{ value: 'value1'} }], 'prop1')).toEqual(true);
+  });
+});
